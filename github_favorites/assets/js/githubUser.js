d03a381415e359a166e18dd21c48716d747ee63b
Iury Henrique <69050176+iury44@users.noreply.github.com>
2024-02-15 20:07:19 -0300

    Add project

---
+++ github_favorites/assets/js/githubUser.js
@@ -0,0 +1,16 @@
+
+//Realiza a busca do usuário dentro da Api do Github
+export class GithubUser{
+  static search(username){
+    const endpoint = `https://api.github.com/users/${username}`
+    return fetch(endpoint)
+    .then(data => data.json())
+    .then(({login,name,public_repos,followers}) =>({
+      login,
+      name,
+      public_repos,
+      followers
+    }))
+
+  }
+}
